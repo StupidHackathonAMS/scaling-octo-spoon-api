0755d76a30a933e92617bf4abe5a09ed6d752a15
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-09-16 13:25:44 +0200

    add emoji support!

--- routes/emoji.js
+++ routes/emoji.js
@@ -1,9 +1,28 @@
 var express = require('express');
 
+var emojiindex = require('./emojindex');
+
+var emojidick = [
+    ["good", "thumbsup"],
+    ["goed", "thumbsup"],
+    ["bad", "thumbsdown"],
+    ["slecht", "thumbsdown"],
+    ["up", "thumbsdown"]
+];
 var router = express.Router();
 
-router.get("/", function(requ, resp){
-    res.json({cookie: "EMOIJIES!"})
+function makeImg(emojicode){
+    return "<img src='"+emojiindex[emojicode]+"' style='height: 1em;'/>"
+}
+
+router.post("/", function(req, res){
+    var cookie = req.body.cookie;
+
+    emojidick.forEach(function (emojitranslater){
+        cookie = cookie.replace(emojitranslater[0], makeImg(emojitranslater[1]))
+    });
+
+    res.json({cookie: cookie})
 });
 
 module.exports = router;
