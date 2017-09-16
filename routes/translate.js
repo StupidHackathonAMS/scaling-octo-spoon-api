5ea0ef751fd187c9a22eb5d8ec55995a75756120
Thijs van de Ven <thijs.van.de.ven@hotmail.com>
2017-09-16 10:46:17 +0200

    added smileys :P

--- routes/translate.js
+++ routes/translate.js
@@ -3,7 +3,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send("yolo")
+    res.send('test')
+});
+
+router.post('/', function (req, res) {
+    var j = req.body;
+    var back = {cookies: []};
+
+    for(var i = 0; i < j.cookies.length; i++){
+        var cook = j.cookies[i];
+        cook.deeg += " :P";
+        back.cookies.push({id: cook.id, deeg: cook.deeg});
+    }
+
+    res.json(back)
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var parser = require('body-parser');
 
 var app = express();
-var testRoute = require('./routes/test');
 
-app.use('/', testRoute);
+app.use(parser.json());
+
+// var testRoute = require('./routes/test');
+var translateRoute = require('./routes/translate');
+
+app.use('/translate', translateRoute);
 
 app.listen(3000);
